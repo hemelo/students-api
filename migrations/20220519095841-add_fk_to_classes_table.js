20dc7881dde4a889b4b04ef7a75bfb1024601a56
Henrique Melo <hmelo2509@gmail.com>
2022-05-19 10:27:15 +0000

    Fk Migrations

---
+++ migrations/20220519095841-add_fk_to_classes_table.js
@@ -0,0 +1,22 @@
+'use strict'
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.addColumn('Classes', 'instructor_id', {
+      allowNull: false,
+      type: Sequelize.INTEGER,
+      references: { model: 'Person', key: 'id' }
+    })
+
+    await queryInterface.addColumn('Classes', 'classType_id', {
+      allowNull: false,
+      type: Sequelize.INTEGER,
+      references: { model: 'ClassType', key: 'id' }
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.removeColumn('Classes', 'instructor_id')
+    await queryInterface.removeColumn('Classes', 'classType_id')
+  }
+}
---
+++ migrations/20220519095905-add_fk_to_enrollments_table.js
@@ -0,0 +1,22 @@
+'use strict'
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.addColumn('Enrollments', 'student_id', {
+      allowNull: false,
+      type: Sequelize.INTEGER,
+      references: { model: 'Person', key: 'id' }
+    })
+
+    await queryInterface.addColumn('Enrollments', 'class_id', {
+      allowNull: false,
+      type: Sequelize.INTEGER,
+      references: { model: 'Class', key: 'id' }
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.removeColumn('Enrollments', 'student_id')
+    await queryInterface.removeColumn('Enrollments', 'class_id')
+  }
+}
