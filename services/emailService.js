45631d4b7ec865056edd44637325a21f7c00c969
Henrique Melo <hmelo2509@gmail.com>
2022-05-20 22:23:00 +0000

    Add email setup for production

--- services/emailService.js
+++ services/emailService.js
@@ -4,21 +4,33 @@ import nodemailer from 'nodemailer'
 
 class Email {
   async sendMail () {
-    const testAccount = await nodemailer.createTestAccount()
+    const login = {
+      host: process.env.MAIL_HOST,
+      auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASSWORD
+      },
+      port: process.env.MAIL_PORT
+    }
+
+    if (!['production', 'prod'].includes(process.env.NODE_ENV)) {
+      const testAccount = await nodemailer.createTestAccount()
+      login.host = 'smtp.ethereal.email'
+      login.auth = testAccount
+      login.port = 587
+    }
 
     const transporter = nodemailer.createTransport({
-      host: 'smtp.ethereal.email',
-      port: 587,
-      secure: false, // True for 465, false for other ports
-      auth: {
-        user: testAccount.user,
-        pass: testAccount.pass
-      }
+      host: login.auth,
+      port: login.port,
+      secure: login.port == 465,
+      auth: login.auth
     })
     const info = await transporter.sendMail(this)
 
-    // Preview only available when sending through an Ethereal account
-    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info))
+    if (!['production', 'prod'].includes(process.env.NODE_ENV)) {
+      console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info))
+    }
   }
 }
 
